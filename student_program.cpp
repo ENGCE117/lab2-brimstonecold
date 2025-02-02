e458f9fbee05e0d2e07802d40e022eba677f36c3
brimstonecold <brimstonecold1@gmail.com>
2025-02-02 22:53:59 +0700

    Update student_program.cpp

--- student_program.cpp
+++ student_program.cpp
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <string.h>
+
+void reverse(char str1[], char str2[]);
 
 int main() {
-    //YOUR CODE HERE
-    return 0 ;
-}//end function
+    char text[50] = "Hello World";
+    char out[50];
+    reverse(text, out);
+    printf("Output: %s\n", out);
+    return 0;
+}
+
+void reverse(char str1[], char str2[]) {
+    int length = strlen(str1);
+    for (int i = 0; i < length; i++) {
+        str2[i] = str1[length - i - 1];
+    }
+    str2[length] = '\0';
+}
