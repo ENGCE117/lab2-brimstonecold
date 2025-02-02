492b91cea00d70514e9352ebb4a9b1e749495c03
brimstonecold <brimstonecold1@gmail.com>
2025-02-02 22:59:25 +0700

    Update student_program.cpp

--- student_program.cpp
+++ student_program.cpp
@@ -1,20 +1,24 @@
 #include <stdio.h>
-#include <string.h>
 
-void reverse(char str1[], char str2[]);
+void reverse(char str1[], char str2[]) ;
 
 int main() {
-    char text[50] = "Hello World";
+    char text[50] = "I Love You";
     char out[50];
+    
     reverse(text, out);
-    printf("Output: %s\n", out);
+
     return 0;
 }
 
 void reverse(char str1[], char str2[]) {
-    int length = strlen(str1);
+    int length = 0;
+    
+    while (str1[length] != '\0') length++ ;
     for (int i = 0; i < length; i++) {
         str2[i] = str1[length - i - 1];
     }
-    str2[length] = '\0';
+
+    printf("word step back : %s\n", str2);  
+
 }
